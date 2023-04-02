cb32fbe0d1c0686d034be015bb3723d6f4270578
ZeLiu1 <90260431+ZeLiu1@users.noreply.github.com>
2023-04-02 21:26:19 -0230

    A6 is done

--- Assignmnet/A6/Assignment 6 ACO Code/Ant.java
+++ Assignmnet/A6/Assignment 6 ACO Code/Ant.java
@@ -1,3 +1,4 @@
+
 /******************************************************************************
  *  ACO for TSP
  *
@@ -24,221 +25,259 @@
 import java.lang.Math;
 import java.util.*;
 
-public class Ant
-{
-    private AntSim sim;           // simulation that this ant belongs to
-    private int [] tour;          // list of nodes in the order they were visited
-    private int tourLength;       // length of tour
-    private int numNodes;         // size of problem
-    
+public class Ant {
+    private AntSim sim; // simulation that this ant belongs to
+    private int[] tour; // list of nodes in the order they were visited
+    private int tourLength; // length of tour
+    private int numNodes; // size of problem
+
     // **** YOU NEED TO COMPLETE THE CODE FOR THE 6 METHODS BELOW ****
-    
-    /* (1)
-    * returns a list of nodes not yet visited by this ant
-    *
-    * input parameter:
-    *    boolean array visited[], where visited[i] == true if node i has been visited by this ant
-    */
-    public ArrayList<Integer> notVisited(boolean visited[])
-    {
+
+    /*
+     * (1)
+     * returns a list of nodes not yet visited by this ant
+     *
+     * input parameter:
+     * boolean array visited[], where visited[i] == true if node i has been visited
+     * by this ant
+     */
+    public ArrayList<Integer> notVisited(boolean visited[]) {
         ArrayList<Integer> notVisited = new ArrayList<Integer>();
-        
+
         // YOUR CODE GOES HERE - REPLACE THIS DUMMY CODE
-        notVisited = AntTester.initIntArray(new int[]{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1});
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                notVisited.add(i);
+            }
+        }
         // END OF YOUR CODE
-        
+
         return notVisited;
     }
-    
-    /* (2)
+
+    /*
+     * (2)
      * returns the lengths of the edges leading to nodes not yet visited by this ant
      *
      * input parameters:
-     *    currentNode: int representing the ant's current position
-     *    notVisited: the list of nodes returned by the getNotVisited() method
+     * currentNode: int representing the ant's current position
+     * notVisited: the list of nodes returned by the getNotVisited() method
      *
      * hints:
-     *    sim.getEdgeLength(i,j) provides the edge length of edge (i,j)
-     *    for each edge: i is the current node, and j is a node not yet visited
+     * sim.getEdgeLength(i,j) provides the edge length of edge (i,j)
+     * for each edge: i is the current node, and j is a node not yet visited
      */
-    public ArrayList<Integer> getEdgeLengths(int currentNode, ArrayList<Integer> notVisited)
-    {
+    public ArrayList<Integer> getEdgeLengths(int currentNode, ArrayList<Integer> notVisited) {
         ArrayList<Integer> lengths = new ArrayList<Integer>();
-        
         // YOUR CODE GOES HERE - REPLACE THIS DUMMY CODE
-        lengths = AntTester.initIntArray(new int[]{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1});
+        for (Integer node : notVisited) {
+            int edgeLength = sim.getEdgeLength(currentNode, node);
+            lengths.add(edgeLength);
+        }
         // END OF YOUR CODE
-        
         return lengths;
     }
-    
-    /* (3)
-    * returns the pheromone levels of the edges leading to nodes not yet visited by this ant
-    *
-    * input parameters:
-    *    currentNode: int representig the ant's current position
-    *    notVisited: the list of nodes returned by the getNotVisited() method
-    *
-    * hint:
-    *    sim.getPheromoneLevel(i,j) provides the pheromone level on edge (i,j)
-    */
-    public ArrayList<Double> getPheromoneLevels(int currentNode, ArrayList<Integer> notVisited)
-    {
-        ArrayList<Double> levels = new ArrayList<Double>();
-        
-        // YOUR CODE GOES HERE - REPLACE THIS DUMMY CODE
-        levels = AntTester.initDoubleArray(new double[]{-1.00,-1.00,-1.00,-1.00,-1.00,-1.00,-1.00,-1.00,-1.00,-1.00});
-        // END OF YOUR CODE
-        
-        return levels;
-    }
-    
-    /* (4)
-     * calculates and returns the probabilities of choosing each edge that leads a node not yet visited
+
+    /*
+     * (3)
+     * returns the pheromone levels of the edges leading to nodes not yet visited by
+     * this ant
      *
      * input parameters:
-     *    levels: the list of pheromone levels returned by the getLevels() method
-     *    lengths: the list of edge lengths returned by the getLevels() method
+     * currentNode: int representig the ant's current position
+     * notVisited: the list of nodes returned by the getNotVisited() method
      *
-     * must use the standard ACO Edge Selection formula, as provided in the class slides:
-     *    p(choose edge i) = (τ^alpha . η^beta) / SUM(τ^alpha . η^beta)
-     *    where for each edge i:
-     *       τ = level of pheromone on an edge
-     *       η = 1/(length of edge i)
+     * hint:
+     * sim.getPheromoneLevel(i,j) provides the pheromone level on edge (i,j)
      */
-    public ArrayList<Double> edgeProbabilities(ArrayList<Double> levels, ArrayList<Integer> lengths)
-    {
-        ArrayList<Double> probabilities = new ArrayList<Double>();
-        
+    public ArrayList<Double> getPheromoneLevels(int currentNode, ArrayList<Integer> notVisited) {
+        ArrayList<Double> levels = new ArrayList<Double>();
+
         // YOUR CODE GOES HERE - REPLACE THIS DUMMY CODE
-        probabilities = AntTester.initDoubleArray(new double[]{0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1});
+        for (Integer node : notVisited) {
+            double pheromoneLevel = sim.getPheromoneLevel(currentNode, node);
+            levels.add(pheromoneLevel);
+        }
         // END OF YOUR CODE
-        
+
+        return levels;
+    }
+
+    /*
+     * (4)
+     * calculates and returns the probabilities of choosing each edge that leads a
+     * node not yet visited
+     *
+     * input parameters:
+     * levels: the list of pheromone levels returned by the getLevels() method
+     * lengths: the list of edge lengths returned by the getLevels() method
+     *
+     * must use the standard ACO Edge Selection formula, as provided in the class
+     * slides:
+     * p(choose edge i) = (τ^alpha . η^beta) / SUM(τ^alpha . η^beta)
+     * where for each edge i:
+     * τ = level of pheromone on an edge
+     * η = 1/(length of edge i)
+     */
+    public ArrayList<Double> edgeProbabilities(ArrayList<Double> levels, ArrayList<Integer> lengths) {
+        ArrayList<Double> probabilities = new ArrayList<Double>();
+        double alpha = 1.0; // Set appropriate value for alpha
+        double beta = 2.0; // Set appropriate value for beta
+        double denominator = 0.0;
+
+        // YOUR CODE GOES HERE - REPLACE THIS DUMMY CODE
+        for (int i = 0; i < levels.size(); i++) {
+            double tau = levels.get(i);
+            double eta = 1.0 / lengths.get(i);
+            double value = Math.pow(tau, alpha) * Math.pow(eta, beta);
+            denominator += value;
+        }
+
+        for (int i = 0; i < levels.size(); i++) {
+            double tau = levels.get(i);
+            double eta = 1.0 / lengths.get(i);
+            double value = Math.pow(tau, alpha) * Math.pow(eta, beta);
+            double probability = value / denominator;
+            probabilities.add(probability);
+        }
+        // END OF YOUR CODE
+
         return probabilities;
     }
-    
-    /* (5)
-    * returns cumulative probabilities of choosing edges
-    *
-    * input parameter:
-    *    probabilities: the list of probabilities returned by the getProbabilities() method
-    */
-    public ArrayList<Double> cumulativeProbabilities(ArrayList<Double> probabilities)
-    {
+
+    /*
+     * (5)
+     * returns cumulative probabilities of choosing edges
+     *
+     * input parameter:
+     * probabilities: the list of probabilities returned by the getProbabilities()
+     * method
+     */
+    public ArrayList<Double> cumulativeProbabilities(ArrayList<Double> probabilities) {
         ArrayList<Double> cumulative = new ArrayList<Double>();
-        
+
         // YOUR CODE GOES HERE - REPLACE THIS DUMMY CODE
-        cumulative = AntTester.initDoubleArray(new double[]{0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0});
+        double cumulativeSum = 0.0;
+        for (Double probability : probabilities) {
+            cumulativeSum += probability;
+            cumulative.add(cumulativeSum);
+        }
         // END OF YOUR CODE
-        
+
         return cumulative;
     }
-    
-    /* (6)
+
+    /*
+     * (6)
      * chooses the next node to visit using roulette wheel selection
      *
      * input parameter:
-     *    cumulative: the list of cumulative proabilities returned by the getCumulativeProbabilities() method
+     * cumulative: the list of cumulative proabilities returned by the
+     * getCumulativeProbabilities() method
      *
      * returns an integer in range [0,n-1] where n = cumulative.size()
      */
-    public int chooseNextNode(ArrayList<Double> cumulative)
-    {
-        // YOUR CODE GOES HERE - REPLACE THIS DUMMY CODE
-        return 0;
-        // END OF YOUR CODE
+    public int chooseNextNode(ArrayList<Double> cumulative) {
+        Random random = new Random();
+        double rand = random.nextDouble();
+
+        for (int i = 0; i < cumulative.size(); i++) {
+            if (rand <= cumulative.get(i)) {
+                return i;
+            }
+        }
+
+        return cumulative.size() - 1;
     }
-    
+
     // **** END OF METHODS TO CODE - DO NOT TOUCH ANY CODE BELOW THIS LINE! ****
-    
+
     // constructor
-    public Ant(AntSim sim, int numNodes)
-    {
+    public Ant(AntSim sim, int numNodes) {
         this.sim = sim;
         this.numNodes = numNodes;
-        tour = new int [numNodes];
-        tourLength = 500;	// placeholder for now
+        tour = new int[numNodes];
+        tourLength = 500; // placeholder for now
     }
-    
+
     // visit every node, record the route taken and return the tour length
-    public int tour()
-    {
+    public int tour() {
         // place this ant on a starting node, chosen at random
         int currentNode = (int) (Math.random() * numNodes);
-        
+
         // initialise the recording of the tour
-        boolean [] visited = new boolean [numNodes];
-        visited [currentNode] = true;
-        tour [0] = currentNode;
+        boolean[] visited = new boolean[numNodes];
+        visited[currentNode] = true;
+        tour[0] = currentNode;
         tourLength = 0;
-        for (int index = 0; index < numNodes; index ++)
-        {
-            if (index != currentNode) { visited [index] = false; }
+        for (int index = 0; index < numNodes; index++) {
+            if (index != currentNode) {
+                visited[index] = false;
+            }
         }
-        
+
         int nodeCount = 1; // how many nodes have been visited so far
-        
+
         // keep going until the tour is complete:
-        while (nodeCount < numNodes)
-        {
+        while (nodeCount < numNodes) {
             // determine which nodes have not yet been visited
             ArrayList<Integer> notVisited = notVisited(visited);
-            
-            // for each of those nodes, get the lengths and pheromone levels of the edges that lead to them
+
+            // for each of those nodes, get the lengths and pheromone levels of the edges
+            // that lead to them
             ArrayList<Integer> lengths = getEdgeLengths(currentNode, notVisited);
             ArrayList<Double> levels = getPheromoneLevels(currentNode, notVisited);
-            
+
             // ...hence get the probability of choosing each edge
-            ArrayList<Double> probabilities = edgeProbabilities( levels, lengths);
-            
+            ArrayList<Double> probabilities = edgeProbabilities(levels, lengths);
+
             // ...hence get the cumulative probabilties
             ArrayList<Double> cumulative = cumulativeProbabilities(probabilities);
-            
+
             // ...and finally choose the next node to visit using roulette wheel selection
             int chosenIndex = chooseNextNode(cumulative);
             int chosen = notVisited.get(chosenIndex);
-            
+
             // update the tour records
             visited[chosen] = true;
             tourLength += sim.getEdgeLength(currentNode, chosen);
             currentNode = chosen;
             tour[nodeCount] = chosen;
-            nodeCount ++;
+            nodeCount++;
         }
-        // there is one final edge to visit, which takes the ant back to its starting node
+        // there is one final edge to visit, which takes the ant back to its starting
+        // node
         tourLength += sim.getEdgeLength(tour[numNodes - 1], 0);
-        
+
         return tourLength;
     }
-    
+
     // lay pheromone inversely proportional to the length of this ant's tour
-    public void layPheromone()
-    {
+    public void layPheromone() {
         // how much pheromone to lay on each edge
         double perEdge = (double) (sim.getTotalPheromone() / tourLength);
-        
+
         // re-trace this ants tour and evenly spread pheromone on each edge
         int previous = 0;
-        for (int index = 1; index < numNodes + 1; index ++)
-        {
+        for (int index = 1; index < numNodes + 1; index++) {
             int current = tour[index % numNodes];
             sim.addPheromone(previous, current, perEdge);
             previous = current;
         }
     }
-    
+
     // return the length of this ant's tour
-    public int getTourLength()  { return tourLength; }
-    
+    public int getTourLength() {
+        return tourLength;
+    }
+
     // print the tour details to console
-    public void printTour()
-    {
+    public void printTour() {
         System.out.println("Tour length of " + tourLength +
-                           ", visiting the nodes in this order:");
-        for (int index = 0; index < tour.length - 1; index ++)
-        {
-            System.out.print(tour [index] + ", ");
+                ", visiting the nodes in this order:");
+        for (int index = 0; index < tour.length - 1; index++) {
+            System.out.print(tour[index] + ", ");
         }
-        System.out.println(tour [tour.length - 1] + ".");
+        System.out.println(tour[tour.length - 1] + ".");
     }
 }
